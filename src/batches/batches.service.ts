8e9957958f452bf7930b6e4999fd64217fdcbf80
kkn1125 <xpfjrnseks1@naver.com>
2025-08-20 23:35:30 +0900

    ✨ feat: batch 도메인 추가

---
+++ src/batches/batches.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BatchesService {
+  // @Cron(CronExpression.EVERY_10_SECONDS)
+  // async handleCron() {
+  //   console.log('Cron job executed');
+  // }
+}
