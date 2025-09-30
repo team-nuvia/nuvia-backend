9d1653fc71402ea194828485a83522c0d2359a0d
kkn1125 <xpfjrnseks1@naver.com>
2025-10-01 01:13:51 +0900

    🛠️ fix: api ssl 적용 로직 추가

--- src/main.ts
+++ src/main.ts
@@ -16,12 +16,21 @@ import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { JwtGuard } from './auth/guard/jwt.guard';
+import { CERT_KEY, IS_PROD, PRIV_KEY } from '@common/variable/environment';
+import fs from 'fs';
 
 cluster.schedulingPolicy = cluster.SCHED_RR; // Round Robin
 
 async function bootstrap() {
+  const key = CERT_KEY ? fs.readFileSync(CERT_KEY) : null;
+  const cert = PRIV_KEY ? fs.readFileSync(PRIV_KEY) : null;
+  const httpsOptions = {
+    key,
+    cert,
+  };
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bufferLogs: true,
+    httpsOptions: IS_PROD ? httpsOptions : undefined,
   });
 
   const commonService = app.get(CommonService);
